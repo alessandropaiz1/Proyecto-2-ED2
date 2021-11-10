2a0aaad921000ddd0b2a503ade1f5b5ce9df810d
alessandropaiz1 <alessandropch@gmail.com>
2021-11-09 22:57:02 -0600

    Se definieron los pines para la pantalla LCD

--- TIVA_C_P2.ino
+++ TIVA_C_P2.ino
@@ -45,3 +45,12 @@ File archivo;
 //Botones
 #define b1   PF_4    // B1 se utiliza para solicitar dato del sensor conectado al ESP32
 #define b2   PF_0    // B2 se utiliza para guardar el dato recibido en la memoria SD
+
+#define LCD_RST PD_0
+#define LCD_CS PD_1
+#define LCD_RS PD_2
+#define LCD_WR PD_3
+#define LCD_RD PE_1
+#define NOTE_C4_1 260
+
+int DPINS[] = {PB_0, PB_1, PB_2, PB_3, PB_4, PB_5, PB_6, PB_7};
